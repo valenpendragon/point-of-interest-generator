1957aab0c3745f15bb77a3d2a461593aee9bbb60
Jeffrey Scott <valenpendragon@gmail.com>
2025-07-16 06:45:06 -0400

    Add import statements for class Dice, return_die_roll, pandas, and numpy. Switch the argument discoverability to discoverability_table, a pd.DataFrame. Add DOCSTRING explaining new parameters and the worksheet format required by class Point_of_Interest.

--- entities/point_of_interest.py
+++ entities/point_of_interest.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+from .dice import Dice
+import pandas as pd
+import numpy as np
 
 
 class Point_of_Interest(ABC):
@@ -7,34 +10,52 @@ class Point_of_Interest(ABC):
     objects. These are places that could be used to create steps in an
     RPG quest cycle, start a whole new one, or add milieu to a world.
 
-    The basic attribute that all points of interest have in common is:
-        discoverability: str    how easily can it found
-
     From there, they branch off in attributes based on the type of POI
     and further subtyping in some instances.
 
+    This class assumes that the DataFrame meets the following criteria:
+        1) It has two columns
+        2) The first column is a string or an integer. The string format
+            is nDm, ndm, or m, d/D are the characters 'd' or 'D'
+            and n, m are integers such that n <+ m and can be converted
+            to integers using int().
+        3) The second column is a string of results  with column header
+            'Result' (capitalization is ignored)
+        4) Column 1 has no numerical gaps from the m in the one row and n
+            in the next row.
+        5) Column headers are: Column 1:  DNor dN, where 'D' or 'd' are the characters
+            and N is an integer, and Column 2: a string
+        6) The first entry in Column 1 should be 1 and the m in the last
+            row must equal N in the Column Header.
     """
-    def __init__(self, discoverability:str, debug=False):
+    def __init__(self, discoverability_table:pd.DataFrame, debug=False):
         """
-        This abstract method assigns the first two
-        :param discoverability:
-        :param poi_type:
+        This abstract method generates the attribute discoverability
+        using the method defined b
+        :param discoverability_table:pd.DataFrame
+        :param debug: bool, defaults to False
         """
-        self.discoverability = discoverability
+        table = discoverability_table
+        cols = list(table.columns)
+        die_size = int(cols[0][1:])
         self.debug = debug
 
 
 class Adventure_Site(Point_of_Interest):
     """
-    This subclass generates the most basic of points of interest,
-    the adventure site. It has the simplest set of attributes:
-        discoverability: str    inherited
-        next_action: str        what the GM should do next
+    This subclass of Point_of_Interest generates the simplest type of POI.
+    an adventure site.
     """
-    def __init__(self, discoverability, next_action='create workup', debug=False):
+    def __init__(self, discoverability_table: pd.DataFrame, next_action='create workup', debug=False):
+        """
+        __init__() requires only discoverability_table.
+        :param discoverability_table: pd.DataFrame
+        :param next_action: str, defaults to 'create workup'
+        :param debug: bool, defaults to False
+        """
         if debug:
-            print(f"Adventure_Site: discoverability: {discoverability}. "
+            print(f"Adventure_Site: discoverability: {discoverability_table}. "
                   f"debug: {debug}")
-        super().__init__(discoverability, debug)
+        super().__init__(discoverability_table, debug)
         self.next_action = next_action
 
